91464c4ef916b2b84224850f8c4cfa471b181ab6
yoro1836 <95689076+yoro1836@users.noreply.github.com>
2025-02-18 09:23:15 +0900

    change props for spoof alu

--- app/src/main/java/com/mrx7014/s24ultraspoofer/MainHook.java
+++ app/src/main/java/com/mrx7014/s24ultraspoofer/MainHook.java
@@ -26,85 +26,32 @@ public class MainHook implements IXposedHookLoadPackage {
 
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {        
-        XposedBridge.log("s24ultraspoofer: Hooking into: " + lpparam.packageName);
-
-        // ro.product.manufacturer
-        XposedHelpers.setStaticObjectField(Build.class, "MANUFACTURER", "Samsung");
-
-        // ro.product.brand
-        XposedHelpers.setStaticObjectField(Build.class, "BRAND", "Samsung");
-
-        // ro.product.device
-        XposedHelpers.setStaticObjectField(Build.class, "DEVICE", "e3q");
-
-        // ro.product.system.device
-        XposedHelpers.setStaticObjectField(Build.class, "SYSTEM.DEVICE", "e3q");
-
-        // ro.build.tags
-        XposedHelpers.setStaticObjectField(Build.class, "TAGS", "release-keys");
-
-        // ro.build.type
-        XposedHelpers.setStaticObjectField(Build.class, "TYPE", "user");
-
-        // ro.product.product
-        XposedHelpers.setStaticObjectField(Build.class, "PRODUCT", "e3q");
-
-        // ro.product.name
-        XposedHelpers.setStaticObjectField(Build.class, "name", "e3q");
-
-        // ro.product.system.name
-        XposedHelpers.setStaticObjectField(Build.class, "name", "e3q");
-
-        // ro.product.vendor.name
-        XposedHelpers.setStaticObjectField(Build.class, "name", "e3q");
-
-        // ro.product.system_ext.name
-        XposedHelpers.setStaticObjectField(Build.class, "name", "e3q");
-
-        // ro.product.system.name
-        XposedHelpers.setStaticObjectField(Build.class, "SYSTEM.NAME", "e3q");
-
-        // ro.product.vendor.name
-        XposedHelpers.setStaticObjectField(Build.class, "VENDOR.NAME", "e3q");
-
-        // ro.product.system_ext.name
-        XposedHelpers.setStaticObjectField(Build.class, "SYSTEM_EXT.NAME", "e3q");
-
+        XposedBridge.log("ALU Spoof: Hooking into: " + lpparam.packageName);
         // ro.system_ext.model
-        XposedHelpers.setStaticObjectField(Build.class, "SYSTEM_EXT.MODEL", "e3q");
+        XposedHelpers.setStaticObjectField(Build.class, "SYSTEM_EXT.MODEL", "VER-N49");
 
         // ro.product.model
-        XposedHelpers.setStaticObjectField(Build.class, "PRODUCT.MODEL", "e3q");
+        XposedHelpers.setStaticObjectField(Build.class, "PRODUCT.MODEL", "VER-N49");
+
+        // ro.product.model
+        XposedHelpers.setStaticObjectField(Build.class, "PRODUCT.PRODUCT.MODEL", "VER-N49");
 
         // ro.product.vendor.model
-        XposedHelpers.setStaticObjectField(Build.class, "VENDOR.MODEL", "e3q");
+        XposedHelpers.setStaticObjectField(Build.class, "VENDOR.MODEL", "VER-N49");
 
         // ro.product.system.model
-        XposedHelpers.setStaticObjectField(Build.class, "SYSTEM.MODEL", "e3q");
+        XposedHelpers.setStaticObjectField(Build.class, "SYSTEM.MODEL", "VER-N49");
 
-        // ro.product.flavor
-        XposedHelpers.setStaticObjectField(Build.class, "FLAVOUR", "e3q-user");
+        // ro.product.system.model
+        XposedHelpers.setStaticObjectField(Build.class, "ODM.MODEL", "VER-N49");
 
-        // ro.soc.model
-        XposedHelpers.setStaticObjectField(Build.class, "SOC.MODEL", "SM8650-AC");
-        
-        // ro.gpu.model
-        XposedHelpers.setStaticObjectField(Build.class, "GPU.MODEL", "Adreno750");
-        
-        // ro.hardware.chipname
-        XposedHelpers.setStaticObjectField(Build.class, "HARDWARE.CHIPNAME", "Pineapple");
-        
-        // ro.build.version.oneui
-        XposedHelpers.setStaticObjectField(Build.class, "BUILD.VERSION,ONEUI", "60101");
+        // ro.product.system.model
+        XposedHelpers.setStaticObjectField(Build.class, "VENDOR_DLKM.MODEL", "VER-N49");
 
-        // ro.product.board
-        XposedHelpers.setStaticObjectField(Build.class, "BOARD", "e3q");
+        // ro.product.model
+        XposedHelpers.setStaticObjectField(Build.class, "PRODUCT.BOOTIMAGE.MODEL", "VER-N49");
 
-        // ro.build.id
-        XposedHelpers.setStaticObjectField(Build.class, "ID", "UP1A.231005.007");
-
-        // ro.build.fingerprint
-        XposedHelpers.setStaticObjectField(Build.class, "FINGERPRINT",
-                "samsung/e3q/e3q:14/UP1A.231005.007/S928BXXS3AXI7:user/release-keys");
+        // ro.product.model
+        XposedHelpers.setStaticObjectField(Build.class, "FACTORY.MODEL", "VER-N49");
     }
 }
